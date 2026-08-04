02abfdde4b27e4334a4e5cf5af30443aa9d6fa56
Thunderkill <66909862+Thunderkill016@users.noreply.github.com>
2026-08-04 11:41:10 +0700

    feat: expose language gate module boundary

---
+++ src/modules/language/index.ts
@@ -0,0 +1,6 @@
+export { buildLanguageWindows } from "./application/build-language-windows";
+export { CheckOriginalEnglish } from "./application/check-original-english";
+export { defaultLanguageEligibilityPolicy } from "./application/default-language-policy";
+export { evaluateLanguageEligibility } from "./application/evaluate-language-eligibility";
+export type { LanguageAnalysisPort } from "./ports/language-analysis-port";
+export type { LanguageEligibilityRepository } from "./ports/language-eligibility-repository";
